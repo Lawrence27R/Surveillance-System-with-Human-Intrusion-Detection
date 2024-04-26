14a4310541b6ec86bafbed6c68ee7a2903134a9a
Lawrence27R <lawrencerodriques06@gmail.com>
2024-04-26 13:52:53 +0530

    dynamic email

--- gui/email_alert.py
+++ gui/email_alert.py
@@ -18,20 +18,22 @@ def send_email(image_filename, object_detected):
     sender = os.environ['EMAIL_SENDER']
     password = os.environ['EMAIL_PASSWORD']
 
-    query = "SELECT * FROM add_details"
+    query = "SELECT email FROM add_details"
     cursor.execute(query)
     rows = cursor.fetchall()
     
-    cc = [row[2] for row in rows]
+    receivers = [row[0] for row in rows]  # Extract email addresses from database rows
+    if not receivers:
+        print("No email addresses found in the add_details table.")
+        return
+
     current_time = datetime.now().strftime("%H:%M:%S - %d/%m/%Y")
-    receiver = "rodriques22062002@gmail.com"
     subject = "Intruder detected - Please check logs and take action."
     email_body = f"<h1>Some stranger is trying to trespass, please ensure and assist. </h1><br>TimeStamp: {current_time}" 
 
     message = MIMEMultipart()
     message['From'] = sender
-    message['To'] = receiver
-    message['cc'] = ', '.join(cc)  # Convert the list to a comma-separated string
+    message['To'] = ', '.join(receivers)  # Convert the list of receivers to a comma-separated string
     message['Subject'] = subject
     message.attach(MIMEText(email_body, 'html'))
 
@@ -43,8 +45,6 @@ def send_email(image_filename, object_detected):
         image = MIMEImage(attachment, name=filename)
         message.attach(image)
 
-    toaddrs = [receiver] + cc
-
     # SMTP connection
     with s.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
@@ -52,7 +52,7 @@ def send_email(image_filename, object_detected):
 
         connection.sendmail(
             from_addr=sender,
-            to_addrs=toaddrs,
+            to_addrs=receivers,
             msg=message.as_string(),
         )
 
@@ -66,10 +66,14 @@ def call():
 
     client = Client(account_sid, auth_token)
 
-    query = "SELECT * FROM add_details"
+    query = "SELECT phone_number FROM add_details"
     cursor.execute(query)
     rows = cursor.fetchall()
-    recpt = '+91' + str(rows[0][3])
+    if not rows:
+        print("No phone numbers found in the add_details table.")
+        return
+
+    recpt = '+91' + str(rows[0][0])
 
     call = client.calls.create(
                             to=recpt,
@@ -78,3 +82,4 @@ def call():
                         )
     
     print(f"Call Initiated: {current_time}")
+
